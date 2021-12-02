a44ca47e0d21d02ff566c4a96eb94b939fb5260b
Bernardo <bernardoafonso9734@gmail.com>
2021-12-02 17:34:21 -0300

    Implementando banco de dados no progama

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 //para iniciar o servidor npm start
 //subir o servidor no ar
 const customExpress = require('./config/customExpress');
-
-const app = customExpress();
-//escutando a porta 3000 que rodara nosso progama
-app.listen(3000, function(){
-    console.log("servidor rodando na porta 3000")
+const Tabelas = require('./infraestrutura/tabelas');
+const conexao = require('./infraestrutura/conexao');
+conexao.connect(erro=>{
+    if(erro){
+        console.log(erro);
+    }else{
+        console.log("Conexao mysql funcionando!");
+        Tabelas.init(conexao);
+        const app = customExpress();
+        //escutando a porta 3000 que rodara nosso progama
+        app.listen(3000, function(){
+        console.log("servidor rodando na porta 3000")
+        });
+    }
 });
