4cec6647b589a21806a015ce681d8f5858054586
Bernardo Afonso <bernardoafonso9734@gmail.com>
2022-01-11 20:46:26 -0300

    Definindos as rota e suas funcoes

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,12 +2,13 @@
 const { process_params } = require('express/lib/router');
 const Atendimento = require('../models/atendimentos');
 module.exports = app => {
-//gerenciando a rota de atendimentos
+//DEFININDO OQUE CADA ROTA IRÁ FAZER
 app.get('/atendimentos', function(req, res){
     //listando os atendimentos
     Atendimento.lista(res);
 });
 app.get('/atendimentos/:id', (req, res)=>{
+    //convertendo o id
     const id = parseInt(req.params.id);
     Atendimento.buscaPorId(id, res);
 });
@@ -15,5 +16,15 @@ app.post('/atendimentos', function(req, res){
     const atendimento = req.body;
     Atendimento.adiciona(atendimento, res);
 });
-
+app.patch('/atendimentos/:id', (req, res)=>{
+    //convertendo o id
+    const id = parseInt(req.params.id);
+    const valores = req.body;
+    Atendimento.altera(id, valores, res);
+});
+app.delete('/atendimentos/:id', (req, res)=>{
+    //convertendo o id
+    const id = parseInt(req.params.id);
+    Atendimento.deleta(id, res);
+});
 }
